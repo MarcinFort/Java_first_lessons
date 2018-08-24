332a0cec2466947ab479aed6d5fa763966fc88f8
Marcin Fortuna <marcin.fort@gmail.com>
2018-08-24 14:36:13 +0200

    Add 'Your Initials' challenge from 'Programming by Doing

---
+++ Programming_by_doing_1_Basics_and_printing/Initials.java
@@ -0,0 +1,13 @@
+public class Initials {
+    
+    public static void main(String[] args) {
+        System.out.println("M   M   FFFF");
+        System.out.println("MM MM   F   ");
+        System.out.println("MM MM   F   ");
+        System.out.println("M M M   FFF ");
+        System.out.println("M   M   F   ");
+        System.out.println("M   M   F   ");
+        System.out.println("M   M   F   ");
+    }
+
+}
